d3fc9a31f16b2188631d1f2e9e4f4394d093ea5d
Miracle Edomwande <miracle.edomwande@xputertechnologies.com>
2019-04-17 12:37:12 +0100

    End of section 12

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -28,7 +28,7 @@ namespace DatingApp.API.Controllers {
         }
 
         [HttpPost ("register")]
-        public async Task<IActionResult> Register (UserForRegisterDto userForRegisterDto) {
+        public async Task<IActionResult> Register ([FromBody]UserForRegisterDto userForRegisterDto) {
             //Validate request
 
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower ();
@@ -36,13 +36,12 @@ namespace DatingApp.API.Controllers {
             if (await _repo.UserExists (userForRegisterDto.Username))
                 return BadRequest ("Username already exists");
 
-            var userToCreate = new User {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register (userToCreate, userForRegisterDto.Password);
-
-            return StatusCode (201);
+            
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+            return CreatedAtRoute("GetUser", new {controller = "Users", id = createdUser.Id}, userToReturn);
 
         }
 
